97b654ea0f9ec46343bf62d5fe0b6cd8dc923b95
Mahfuz <mahfuzru5634@gmail.com>
2025-07-28 13:53:00 +0600

    first commit

---
+++ task1.js
@@ -0,0 +1,8 @@
+let burgerPrice=500;
+
+if(burgerPrice>=500){
+ console.log("Coke is Free");
+}
+else{
+    console.log("Coke=30Tk");
+}
---
+++ task3.js
@@ -0,0 +1,8 @@
+let num1,num2;
+num1=10;
+num2=20;
+
+
+let result;
+(num1>num2)?result=num1*2:result=num1+num2;
+console.log(result); 
